c55f25d2805c35d08a3e5380beb500fda2d581cf
nitesh1012 <thotanitesh5@gmail.com>
2024-12-09 14:26:28 -0500

    Integration commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api', // Backend Spring Boot base URL
+    baseURL: 'http://ec2-3-226-121-227.compute-1.amazonaws.com:32178/api', // Backend Spring Boot base URL
 });
 
 export default instance;
